92ed3300ffff85548662d8428e0c86c0b44dd186
robel yemane <git@robel.slmail.me>
2023-10-19 23:03:53 +0100

    initial commit

--- main.go
+++ main.go
@@ -1,19 +1,41 @@
 package main
 
 import (
+	"context"
+	"log"
 	"net/http"
 	"os"
 
 	"github.com/robel-yemane/rest-api/types"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 
 	echo "github.com/labstack/echo/v4"
 )
 
 // album slice to seed record album data.
 var albums = []types.Album{
-	{ID: "1", Title: "Ygermena'lo", Artist: "Yohannes Tikabo", Price: 30},
+	{ID: "1", Title: "Ygermenalo", Artist: "Yohannes Tikabo", Price: 30},
 	{ID: "2", Title: "Semai", Artist: "Abraham Afewerki", Price: 40},
 	{ID: "3", Title: "Lilo", Artist: "Temesgen Yared", Price: 50},
+}
+var collection *mongo.Collection
+var client *mongo.Client
+
+func init() {
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	var err error
+	client, err = mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Connected to MongoDB!")
+	err = client.Ping(context.Background(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Connected to pinged MongoDB!")
+
 }
 
 // // getAlbums responds with the list of all albums as JSON
@@ -25,16 +47,22 @@ func getAlbums(c echo.Context) error {
 // parameter sent by the client, then returns that album as a response.
 func getAlbumByID(c echo.Context) error {
 	id := c.Param("id")
+	var album types.Album
+	err := collection.FindOne(context.TODO(), types.Album{ID: id}).Decode(&album)
+	if err != nil {
 
-	// loop over the list of albums, looking for
-	// an album whose ID value matches the parameter.
-	for _, a := range albums {
-		if a.ID == id {
-			return c.JSON(http.StatusOK, a)
-
-		}
+		return c.JSON(http.StatusNotFound, "album not found")
 	}
-	return c.JSON(http.StatusNotFound, "album not found")
+	return c.JSON(http.StatusOK, album)
+	// // loop over the list of albums, looking for
+	// // an album whose ID value matches the parameter.
+	// for _, a := range albums {
+	// 	if a.ID == id {
+	// 		return c.JSON(http.StatusOK, a)
+
+	// 	}
+	// }
+	// return c.JSON(http.StatusNotFound, "album not found")
 
 }
 
@@ -45,12 +73,18 @@ func postAlbums(c echo.Context) error {
 	if err := c.Bind(&newAlbum); err != nil {
 		return err
 	}
+	insertResult, err := collection.InsertOne(context.Background(), newAlbum)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Inserted a single document: ", insertResult.InsertedID)
 	albums = append(albums, newAlbum)
 	return c.JSON(http.StatusCreated, newAlbum)
 }
 
 func main() {
 	port := getPort("APP_PORT")
+	collection = client.Database("albumstore").Collection("albums")
 
 	e := echo.New()
 	e.GET("/albums", getAlbums)
--- types/types.go
+++ types/types.go
@@ -1,8 +1,8 @@
 package types
 
 type Album struct {
-	ID     string  `json:"id"`
-	Title  string  `json:"title"`
-	Artist string  `json:"artist"`
-	Price  float64 `json:"price"`
+	ID     string  `json:"id,omitempty" bson:"_id,omitempty`
+	Title  string  `json:"title,omitempty" bson:"title,omitempty"`
+	Artist string  `json:"artist",omitempty bson:"artist,omitempty"`
+	Price  float64 `json:"price",omitempty bson:"price,omitempty"`
 }
